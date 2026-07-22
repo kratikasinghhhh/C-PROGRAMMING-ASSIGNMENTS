69b462743b0b0439588b34b4656c30860b3348aa
kratikasinghhhh <kratikasinghhhh0207@gmail.com>
2026-07-22 14:14:40 +0530

    Add files via upload

---
+++ struct_matrix.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+
+    int matrix[rows][cols];
+
+    // Input
+    printf("\nEnter elements of the matrix:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("Element [%d][%d]: ", i, j);
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // Output
+    printf("\nMatrix is:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%4d ", matrix[i][j]);  // formatted spacing
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
